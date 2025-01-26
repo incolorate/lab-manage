2c81b26dee8b264adaf38fa64cc44beb42285848
incolorate <negrupaul59@gmail.com>
2025-01-26 09:46:28 +0200

    Prime vue init

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,8 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+import Button from "primevue/button";
+import CascadeSelect from "primevue/cascadeselect";
 </script>
 
 <template>
@@ -8,22 +10,26 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
-                    </div>
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <Button label="Verify" />
                 </div>
+                <CascadeSelect
+                    v-model="selectedCity"
+                    :options="countries"
+                    optionLabel="cname"
+                    optionGroupLabel="name"
+                    :optionGroupChildren="['states', 'cities']"
+                    class="w-56"
+                    placeholder="Select a City"
+                />
             </div>
         </div>
     </AuthenticatedLayout>
